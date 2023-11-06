b282bf04f53e59986b04d6177223e43c5fd3f3f5
ViniciusBraga1 <141172810+ViniciusBraga1@users.noreply.github.com>
2023-11-06 16:46:56 -0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,15 @@
+def simples(cor):
+    if cor =='azul':
+        return'escolheu certo'
+        
+def medio(cor):
+    if cor =='azul':
+        return 'tente outra cor'
+    
+def completo(cor):
+    if cor =='azul':
+        return 'escolheu certo'
+    elif cor =='marrom':
+        return 'não tem salvação'
+    else:
+        return 'tente outra cor'
